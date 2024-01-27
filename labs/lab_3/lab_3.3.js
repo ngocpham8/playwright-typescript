2c2046dce517352c2944bfe761cf332ce3e84aee
ngocpham8 <ngocpb8@gmail.com>
2024-01-27 21:17:45 +0700

    done 3.4

--- labs/lab_3/lab_3.3.js
+++ labs/lab_3/lab_3.3.js
@@ -1,8 +1,9 @@
 //Do NOT use any built-in sort method. Please write a sort logic to sort an integer array from min to max
 
+//1. Bubble sort
 let intArr = [12, 34, 1, 16, 28]
-//Compare 2 value. if a > b then swap b -> a for correct postion
 for (let i = 0; i < intArr.length; i++) {
+    // Last i elements are already in place  
     for (let j = 0; j < intArr.length - 1 - i; j++) {
         if (intArr[j] > intArr[j + 1]) {
             let temp = intArr[j];
@@ -12,4 +13,3 @@ for (let i = 0; i < intArr.length; i++) {
     }
 }
 console.log('Sorted array:', intArr);
-
--- labs/lab_3/lab_3.4.js
+++ labs/lab_3/lab_3.4.js
@@ -1,17 +1,42 @@
 //Merge 2 SORTED integer array into one SORTED array
 
+// // Solution 1: Combine 2 array then sort
+// let intArr1 = [1, 12, 16, 28, 34, 105, 108, 120, 125]
+// let intArr2 = [1, 2, 13, 16, 27, 99, 100, 103]
+// let combineArr = intArr1.concat(intArr2)
+// for (let i = 0; i < combineArr.length; i++) {
+//     for (let j = 0; j < combineArr.length - 1 - i; j++) {
+//         if (combineArr[j] > combineArr[j + 1]) {
+//             let temp = combineArr[j];
+//             combineArr[j] = combineArr[j + 1];
+//             combineArr[j + 1] = temp;
+//         }
+//     }
+// }
+// console.log('Sorted array:', combineArr);
+
+// Solution 2: check each value of intArr1 and insert into correct postion of intArr2
 let intArr1 = [1, 12, 16, 28, 34, 105, 108, 120, 125]
 let intArr2 = [1, 2, 13, 16, 27, 99, 100, 103]
-
-// // Solution 1: Combine 2 array then sort
-let combineArr = intArr1.concat(intArr2)
-for (let i = 0; i < combineArr.length; i++) {
-    for (let j = 0; j < combineArr.length - 1 - i; j++) {
-        if (combineArr[j] > combineArr[j + 1]) {
-            let temp = combineArr[j];
-            combineArr[j] = combineArr[j + 1];
-            combineArr[j + 1] = temp;
+while (intArr1.length > 0) {
+    let isFoundPosition = false;
+    let j = 0;
+    while (!isFoundPosition) {
+        if (intArr1[0] <= intArr2[j]) {
+            intArr2.splice(j, 0, intArr1[0]);
+            isFoundPosition = true;
         }
+        else if (intArr1[0] > intArr2[j] && intArr1[0] <= intArr2[j + 1]) {
+            intArr2.splice(j + 1, 0, intArr1[0]);
+            isFoundPosition = true;
+        }
+        else if (intArr1[0] > intArr2[intArr2.length - 1]) {
+            intArr2.push(intArr1[0]);
+            isFoundPosition = true;
+        }
+        j++;
     }
+    intArr1.shift();
 }
-console.log('Sorted array:', combineArr);
+
+console.log('Sorted array:', intArr2);
