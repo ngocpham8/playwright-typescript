f75191eff300c8c90d6e3c4b106a846507760feb
ngocpham8 <ngocpb8@gmail.com>
2024-01-28 22:07:09 +0700

    Update feedback for 3.1

--- labs/lab_3/lab_3.1.js
+++ labs/lab_3/lab_3.1.js
@@ -2,10 +2,22 @@
 
 
 let intArr = [1, 2, 3, 4, 5, 6, 8, 10, 11];
+
+// 1. Use filter
+let evenArr = intArr.filter(getEvenNumbers);
+
+function getEvenNumbers(value, index, array) {
+    return value % 2 === 0;
+}
+
+evenNumberCount = evenArr.length; 
+console.log('Even numbers:', evenNumberCount);
+console.log('Odd numbers:', intArr.length - evenNumberCount);
+
 // let evenNumberCount = 0;
 // let oddNumberCount = 0;
 
-// 1. Use for loop
+// 2. Use for loop
 // for (let index = 0; index < intArr.length; index++) {
 //     if (intArr[index] % 2 === 0) {
 //         evenNumberCount += 1;
@@ -17,13 +29,3 @@ let intArr = [1, 2, 3, 4, 5, 6, 8, 10, 11];
 // console.log('Odd numbers:', oddNumberCount);
 
 
-// 2. Use filter
-let evenArr = intArr.filter(getEvenNumbers);
-
-function getEvenNumbers(value, index, array) {
-    return value % 2 === 0;
-}
-
-evenNumberCount = evenArr.length; 
-console.log('Even numbers:', evenNumberCount);
-console.log('Odd numbers:', intArr.length - evenNumberCount);
